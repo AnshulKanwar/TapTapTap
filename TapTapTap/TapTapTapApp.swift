233d77866c467b02aa8d2a3c108ab2f4b5ff0c59
Anshul Kanwar <4n5hu1@protonmail.com>
2022-11-02 19:46:33 +0530

    Initial Commit

---
+++ TapTapTap/TapTapTapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapTapTapApp.swift
+//  TapTapTap
+//
+//  Created by Anshul Kanwar on 02/11/22.
+//
+
+import SwiftUI
+
+@main
+struct TapTapTapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
