4f7c74b25ed0a488283439d165b1150ec79bb468
Anshul Kanwar <4n5hu1@protonmail.com>
2022-11-02 21:38:28 +0530

    game completed

---
+++ TapTapTap/MainView.swift
@@ -0,0 +1,73 @@
+//
+//  ContentView.swift
+//  TapTapTap
+//
+//  Created by Anshul Kanwar on 02/11/22.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    @State var viewHeight = 0.0
+    @State var fraction = 0.5
+    @State var isOver = false
+    @State var winner = ""
+    
+    var redHeight: Double {
+        fraction * viewHeight
+    }
+    
+    var blueHeight: Double {
+        (1 - fraction) * viewHeight
+    }
+    
+    var body: some View {
+        GeometryReader { geo in
+            ZStack {
+                VStack {
+                    Rectangle()
+                        .fill(.red)
+                        .frame(height: redHeight)
+                        .onTapGesture {
+                            fraction += 0.01
+                            if fraction >= 0.95 {
+                                isOver = true
+                                winner = "Red"
+                            }
+                        }
+                    Rectangle()
+                        .fill(.blue)
+                        .frame(height: blueHeight)
+                        .onTapGesture {
+                            fraction -= 0.01
+                            if fraction <= 0.05 {
+                                isOver = true
+                                winner = "Blue"
+                            }
+                        }
+                }
+                .onAppear {
+                    viewHeight = geo.size.height
+                }
+            }
+        }
+        .ignoresSafeArea()
+        .alert(isPresented: $isOver) {
+            Alert(
+                title: Text("Game Over"),
+                message: Text("\(winner) won"),
+                dismissButton: .default(Text("Play Again"), action: {
+                    isOver = false
+                    fraction = 0.5
+                    winner = ""
+                })
+            )
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
